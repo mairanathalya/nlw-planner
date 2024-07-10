df2faee617f9364349c2ad22ff43d2b0ca9be57a
mairanathalya <maira.nathalya@gmail.com>
2024-07-10 14:04:44 -0300

    feat: create participant

--- src/main/java/com/rockeseat/planner/participant/ParticipantService.java
+++ src/main/java/com/rockeseat/planner/participant/ParticipantService.java
@@ -1,2 +1,15 @@
-package com.rockeseat.planner.participant;public class ParticipantService {
+package com.rockeseat.planner.participant;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParticipantService {
+
+    public void registerParticipantToTrip(List<String> participantsToInvite, UUID tripId){
+
+    }
+
 }
